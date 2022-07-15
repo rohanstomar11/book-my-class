eddb4278174e5b6b23029865e645afabef961569
Rohan Singh <rohanstomar11@gmail.com>
2022-07-16 01:09:05 +0530

    added vector icons

--- src/screen/splash.js
+++ src/screen/splash.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, ScrollView, TouchableOpacity } from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const SplashScreen = ({navigation}) => {
   return (
@@ -12,11 +13,12 @@ const SplashScreen = ({navigation}) => {
         style={styles.button}
         activeOpacity={0.8}
         onPress={() => {navigation.goBack()}}>
-          <Text style={styles.buttonText}>Go Back</Text>
+        <Icon name='arrowleft' size={24} color={'#F0FEFE'} />
+        <Text style={styles.buttonText}>Go Back</Text>
       </TouchableOpacity>
     </ScrollView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -31,18 +33,20 @@ const styles = StyleSheet.create({
     fontWeight: '800',
   },
   button: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
     backgroundColor: '#3036D6',
     paddingVertical: 10,
     paddingHorizontal: 40,
     borderRadius: 12,
     elevation: 12,
-    justifyContent: 'center',
-    alignItems:'center'
   },
   buttonText: {
     color: '#F0FEFE',
     fontSize: 20,
     fontWeight: '600',
+    marginLeft: 10,
   }
 });
 
