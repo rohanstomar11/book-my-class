d42f2008db34b9520e79126341fcb59faf7e2f05
RohanSinghTomar <rohansingh.tomar@thecareerlabs.com>
2022-08-09 00:44:58 +0530

    layout from firestore done - one floor

--- src/component/room.js
+++ src/component/room.js
@@ -1,7 +1,7 @@
 import {TouchableOpacity, Text, StyleSheet} from 'react-native';
 import React, {useState} from 'react';
-import { FONTS } from '../assets/fontFamily';
-import { COLORS } from '../assets/color';
+import {FONTS} from '../assets/fontFamily';
+import {COLORS} from '../assets/color';
 
 export default function ClassRoom({classroomnum, booked = false}) {
   const [selected, setSelected] = useState(false);
@@ -10,10 +10,15 @@ export default function ClassRoom({classroomnum, booked = false}) {
     <TouchableOpacity
       style={[
         styles.classContainer,
-        {backgroundColor: booked ? COLORS.red95 : selected ? COLORS.green95 : COLORS.grey},
-        {width : selected ? 75 : 70},
-        {height : selected ? 55 :50 }
-        
+        {
+          backgroundColor: booked
+            ? COLORS.red95
+            : selected
+            ? COLORS.green95
+            : COLORS.grey,
+        },
+        {width: selected ? 62 : 60},
+        {height: selected ? 62 : 60},
       ]}
       activeOpacity={0.75}
       onPress={() => {
@@ -21,12 +26,14 @@ export default function ClassRoom({classroomnum, booked = false}) {
         console.log('Booked');
       }}
       disabled={booked}>
-
-      <Text style={[styles.roomNumText ,
-      {fontFamily : selected ? FONTS.Bold : FONTS.Regular},
-      {fontSize :  selected ? 16 : 15 },
-      {color: booked ? COLORS.white :'black'}] }>{classroomnum}
-      
+      <Text
+        style={[
+          styles.roomNumText,
+          {fontFamily: selected ? FONTS.Bold : FONTS.Regular},
+          {fontSize: selected ? 16 : 15},
+          {color: booked ? COLORS.white : 'black'},
+        ]}>
+        {classroomnum}
       </Text>
     </TouchableOpacity>
   );
@@ -36,17 +43,13 @@ const styles = StyleSheet.create({
   classContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    width: 70,
-    height: 50,
-    padding: 10,
     borderRadius: 4,
     borderWidth: 2,
     borderColor: '#C4C4C8',
-    margin: 5,
+    margin: '2%',
   },
   roomNumText: {
-    
     color: 'black',
-    fontFamily:FONTS.Regular
+    fontFamily: FONTS.Regular,
   },
 });
--- src/screen/home.js
+++ src/screen/home.js
@@ -1,22 +1,56 @@
-import React from 'react';
-import {StyleSheet, StatusBar, SafeAreaView, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, StatusBar, SafeAreaView, View, Text} from 'react-native';
 import {COLORS} from '../assets/color';
 import Header from '../component/header';
 import ClassRoom from '../component/room';
+import firestore from '@react-native-firebase/firestore';
 
 const HomeScreen = ({navigation}) => {
+  const [roomCount, setRoomCount] = useState(0);
+  const [floor, setFloor] = useState([]);
+
+  useEffect(() => {
+    firestore()
+      .collection('floors')
+      .doc('0')
+      .get()
+      .then(
+        response => {
+          setRoomCount(response._data?.room_count);
+        },
+        error => {
+          console.error(error);
+        },
+      );
+    for (let i = 1; i <= 25; i++) {
+      if (Object.keys(floor).length <= roomCount) {
+        if (i < 10) {
+          let item = '00' + i.toString();
+          setFloor(floor => [...floor, item]);
+        } else {
+          let item = '0' + i.toString();
+          setFloor(floor => [...floor, item]);
+        }
+      }
+    }
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Header navigation={navigation} />
       <StatusBar backgroundColor={COLORS.primary} barStyle="light-content" />
-      <View style={{flexDirection:'row'}}>
-      <ClassRoom classroomnum={'001'}/>
-      <ClassRoom classroomnum={'002'} booked={true}/>
-      <ClassRoom classroomnum={'003'}/>
-      <ClassRoom classroomnum={'004'}/>
-      
+      <View
+        style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          marginTop: '5%',
+        }}>
+        {floor.map((item, index) => {
+          return <ClassRoom classroomnum={item} key={item} />;
+        })}
       </View>
-      
     </SafeAreaView>
   );
 };
