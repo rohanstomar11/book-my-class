9d05251c8c678c90886238d12cb8fa5228a74dcb
RohanSinghTomar <rohansingh.tomar@thecareerlabs.com>
2022-08-09 02:00:45 +0530

    minor

--- src/component/header.js
+++ src/component/header.js
@@ -1,14 +1,19 @@
-import React,{useState} from 'react';
-import {Text, View, Image, TouchableOpacity, StyleSheet, Platform, Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Platform,
+} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import {FONTS} from '../assets/fontFamily';
 import {COLORS} from '../assets/color';
 import Icon from 'react-native-vector-icons/AntDesign';
 import {Dropdown} from 'react-native-element-dropdown';
-import DatePicker from 'react-native-date-picker';
-import { template } from '@babel/core';
 
 const data = [
+  {label: 'Floor 0', value: '1'},
   {label: 'Floor 1', value: '1'},
   {label: 'Floor 2', value: '2'},
   {label: 'Floor 3', value: '3'},
@@ -16,39 +21,36 @@ const data = [
   {label: 'Floor 5', value: '5'},
 ];
 
-const timeData = [
-  {label: '10:00am- 11:00am', value: '1'},
-  {label: '11:00am- 12:00pm', value: '2'},
-  {label: '1:00pm- 2:00pm', value: '3'},
-  {label: '2:00pm- 3:00pm', value: '4'},
-  {label: '3:00pm- 4:00pm', value: '5'},
-];
-
-
-
+// const timeData = [
+//   {label: '10:00am- 11:00am', value: '1'},
+//   {label: '11:00am- 12:00pm', value: '2'},
+//   {label: '1:00pm- 2:00pm', value: '3'},
+//   {label: '2:00pm- 3:00pm', value: '4'},
+//   {label: '3:00pm- 4:00pm', value: '5'},
+// ];
 
 export default function Header({navigation}) {
   const [value, setValue] = useState(null);
   const [isFocus, setIsFocus] = useState(false);
 
-  const [date, setDate] = useState(new Date())
-const [mode, setMode] = useState('date')
-const [show,setShow] = useState(false)
-const [test,setText] = useState('Empty')
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+  const [test, setText] = useState('Empty');
 
-const onChange = (event,selectedDate) => {
-  const currentDate = selectedDate || date;
-  setShow(Platform.OS === 'ios')
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
 
-  // let tempDate = new Date(currentDate);
-  // let  fDate = tempDate.getDate() + '/' + (tempDate.getMonth()+1) + '/' + tempDate.getFullYear();
-  
-}
+    // let tempDate = new Date(currentDate);
+    // let  fDate = tempDate.getDate() + '/' + (tempDate.getMonth()+1) + '/' + tempDate.getFullYear();
+  };
 
-const showMode = (currentMode) => {
-  setShow(true);
-  setMode(currentMode);
-}
+  const showMode = currentMode => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
   return (
     <View style={styles.rootcontainer}>
       <View style={styles.firstcont}>
@@ -64,7 +66,7 @@ const showMode = (currentMode) => {
           style={styles.dateDropdown}
           placeholderStyle={styles.placeholderStyle}
           selectedTextStyle={styles.selectedTextStyle}
-          iconStyle={styles.iconStyle} 
+          iconStyle={styles.iconStyle}
           data={data}
           maxHeight={300}
           labelField="label"
@@ -101,40 +103,42 @@ const showMode = (currentMode) => {
         </TouchableOpacity>
       </View>
 
-      <View style={styles.secondcont}>
-        <TouchableOpacity style={styles.Datecontainer} placeholder ='Select Date' onPress={() => showMode('date')}>
-          <Text>Select Date</Text></TouchableOpacity>
-      </View>
+      {/* <View style={styles.secondcont}>
+        <TouchableOpacity
+          style={styles.Datecontainer}
+          placeholder="Select Date"
+          onPress={() => showMode('date')}>
+          <Text>Select Date</Text>
+        </TouchableOpacity>
+      </View> */}
 
-      {show && (
-        <DatePicker 
-        testID='datePicker'
-        value={date}
-        mode={mode}
-        
-        displaye='default'
-        onChange={onChange}
+      {/* {show && (
+        <DatePicker
+          testID="datePicker"
+          value={date}
+          mode={mode}
+          displaye="default"
+          onChange={onChange}
         />
-      )}
+      )} */}
 
-<Dropdown
-          style={styles.timeDropdown}
-          placeholderStyle={styles.placeholderStyle}
-          selectedTextStyle={styles.selectedTextStyle}
-          data={timeData}
-          maxHeight={300}
-          labelField="label"
-          valueField="value"
-          placeholder="Select Time Slot"
-          value={value}
-          onFocus={() => setIsFocus(true)}
-          onBlur={() => setIsFocus(false)}
-          onChange={item => {
-            setValue(item.value);
-            setIsFocus(false);
-          }}
-        />
-      
+      {/* <Dropdown
+        style={styles.timeDropdown}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        data={timeData}
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder="Select Time Slot"
+        value={value}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={item => {
+          setValue(item.value);
+          setIsFocus(false);
+        }}
+      /> */}
     </View>
   );
 }
@@ -142,7 +146,7 @@ const showMode = (currentMode) => {
 const styles = StyleSheet.create({
   rootcontainer: {
     flexDirection: 'column',
-    paddingVertical:10,
+    paddingVertical: 10,
     backgroundColor: COLORS.primary,
     paddingHorizontal: 20,
     width: '100%',
@@ -150,7 +154,7 @@ const styles = StyleSheet.create({
   firstcont: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems:'center'
+    alignItems: 'center',
   },
   secondcont: {
     flexDirection: 'row',
@@ -164,7 +168,6 @@ const styles = StyleSheet.create({
     borderColor: COLORS.white,
     borderWidth: 0,
   },
- 
 
   floorcontainer: {
     backgroundColor: COLORS.white,
@@ -194,37 +197,34 @@ const styles = StyleSheet.create({
     height: 50,
     borderBottomColor: 'black',
     borderBottomWidth: 0.5,
-    backgroundColor:COLORS.white,
-    width:'60%',
-    borderRadius:5
+    backgroundColor: COLORS.white,
+    width: '60%',
+    borderRadius: 5,
   },
   iconStyle: {
     width: 30,
     height: 30,
-    
   },
- 
   placeholderStyle: {
     fontSize: 16,
-    color:COLORS.primary,
-    fontFamily:FONTS.SemiBold,
-    paddingLeft:10
+    color: COLORS.primary,
+    fontFamily: FONTS.SemiBold,
+    paddingLeft: 10,
   },
   selectedTextStyle: {
     fontSize: 16,
-    color:COLORS.primary,
-    fontFamily:FONTS.SemiBold,
-    paddingLeft:10
+    color: COLORS.primary,
+    fontFamily: FONTS.SemiBold,
+    paddingLeft: 10,
   },
-  
+
   timeDropdown: {
     margin: 16,
     height: 30,
     borderBottomColor: 'black',
     borderBottomWidth: 0.5,
-    backgroundColor:COLORS.white,
-    width:'50%',
-    borderRadius:5
+    backgroundColor: COLORS.white,
+    width: '50%',
+    borderRadius: 5,
   },
- 
 });
