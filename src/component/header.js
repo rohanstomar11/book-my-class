a5e6f8ca30be001f614f6392ff540b7bf47e2337
RohanSinghTomar <rohansingh.tomar@thecareerlabs.com>
2022-09-04 08:43:11 +0530

    deisgn fix for logout modal

--- src/component/header.js
+++ src/component/header.js
@@ -1,5 +1,12 @@
 import React, {useState} from 'react';
-import {View, Image, TouchableOpacity, StyleSheet, Text,Modal} from 'react-native';
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  Modal,
+} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import {FONTS} from '../assets/fontFamily';
 import {COLORS} from '../assets/color';
@@ -39,52 +46,42 @@ export default function Header({
         <TouchableOpacity
           activeOpacity={0.8}
           onPress={() => {
-            // auth()
-            //   .signOut()
-            //   .then(
-            //     () => {
-            //       navigation.replace('LoginScreen');
-            //     },
-            //     error => {
-            //       console.error(error);
-            //     },
-            //   );
-            setModalVisible(true)
+            setModalVisible(true);
           }}>
           <Icon
             name="setting"
-            size={45}
+            size={40}
             color={COLORS.white}
             style={styles.user}
-            
           />
 
           <Modal
             animationType="fade"
             transparent={true}
             visible={modalVisible}
+            statusBarTranslucent={true}
             onRequestClose={() => {
               setModalVisible(!modalVisible);
             }}>
-              <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <TouchableOpacity
-              onPress={()=>{
-              auth()
-              .signOut()
-              .then(
-                () => {
-                  navigation.replace('LoginScreen');
-                },
-                error => {
-                  console.error(error);
-                },
-              );
-            }
-          }>
-            
-            <Text style={styles.logoutText}>Log Out</Text></TouchableOpacity>
-            </View>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <TouchableOpacity
+                  style={{flex: 1}}
+                  onPress={() => {
+                    auth()
+                      .signOut()
+                      .then(
+                        () => {
+                          navigation.replace('LoginScreen');
+                        },
+                        error => {
+                          console.error(error);
+                        },
+                      );
+                  }}>
+                  <Text style={styles.logoutText}>Log Out</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </Modal>
         </TouchableOpacity>
@@ -160,27 +157,25 @@ const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
     backgroundColor: 'rgba(0,0,0,0.5)',
-    alignItems:'flex-end',
   },
   modalView: {
-    marginTop: 80,
-    marginLeft:20,
+    marginTop: '25%',
+    alignSelf: 'center',
     backgroundColor: 'white',
-    borderRadius: 20,
-    paddingVertical:20,
-    elevation: 5,
-    borderWidth:1,
-    borderColor:COLORS.primary,
-    width:'100%',
-    alignItems:'center',
-    backgroundColor:'white'
+    borderRadius: 8,
+    paddingVertical: 16,
+    elevation: 16,
+    borderWidth: 1,
+    borderColor: COLORS.primary,
+    width: '95%',
+    height: '8%',
+    alignItems: 'center',
   },
-  logoutText:{
-    fontFamily:FONTS.Bold,
-    fontSize:18,
-    color:COLORS.primary,
-    letterSpacing:0.7,
-    
+  logoutText: {
+    fontFamily: FONTS.Bold,
+    fontSize: 18,
+    color: COLORS.primary,
+    letterSpacing: 0.7,
   },
   placeholderStyle: {
     fontSize: 16,
@@ -216,7 +211,7 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   user: {
-    marginRight: 3,
+    marginRight: '2%',
   },
   datetext: {
     fontSize: 16,
